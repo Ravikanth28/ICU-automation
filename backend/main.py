050ce88a25bf5b278ac77a71d2a0e1ba41652ad6
Ravikanth <ravikanthsankaran@gmail.com>
2026-01-18 20:13:39 +0530

    UI enhanced

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from backend.database import engine
 from backend import models
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from backend.routes import patients, resources, allocation
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Smart ICU Resource Allocation")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(patients.router)
 app.include_router(resources.router)
--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -1,9 +1,33 @@
+const BASE_URL = "http://127.0.0.1:8000";
+
+function addPatient() {
+    const name = document.getElementById("pname").value;
+    const severity = document.getElementById("severity").value;
+
+    fetch(`${BASE_URL}/patients/?name=${name}&severity=${severity}`, {
+        method: "POST"
+    })
+    .then(res => res.json())
+    .then(data => show(data));
+}
+
+function addResource() {
+    fetch(`${BASE_URL}/resources/?resource_type=ICU_BED`, {
+        method: "POST"
+    })
+    .then(res => res.json())
+    .then(data => show(data));
+}
+
 function allocate() {
-  fetch("http://127.0.0.1:8000/allocate/", {
-    method: "POST"
-  })
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById("output").innerText = JSON.stringify(data, null, 2);
-  });
+    fetch(`${BASE_URL}/allocate/`, {
+        method: "POST"
+    })
+    .then(res => res.json())
+    .then(data => show(data));
+}
+
+function show(data) {
+    document.getElementById("output").innerText =
+        JSON.stringify(data, null, 2);
 }
